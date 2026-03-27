fde4cda5ecc37962491748dcaaa2730ed8050ec9
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2026-03-27 15:30:34 -0500

    feat(board): add board rendering logic

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 import pygame as pg
+from board import Board
 
 pg.init()
 
 class Game: 
   def __init__(self):
-    self.WINDOW_WIDTH = 1280
+    self.WINDOW_WIDTH = 720
     self.WINDOW_HEIGHT = 720
 
+    self.board = Board() 
     self.window = pg.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
 
     self.running = False
@@ -18,3 +20,7 @@ class Game:
       for e in pg.event.get(): 
         if e.type == pg.QUIT: 
           self.running = False
+
+      self.board.draw(self.window)
+
+      pg.display.flip()
