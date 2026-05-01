d9e5d0fadae50de6d4b9f8275e105547586606da
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2026-04-30 22:40:15 -0500

    feat(king): add king piece
    can't believe the pawn was one of the hardest to code

---
+++ pieces/king.py
@@ -0,0 +1,38 @@
+from base.piece import Piece
+
+class King(Piece): 
+  def __init__(self, path, width, height):
+    super().__init__(path, width, height)
+
+  def get_moves(self, board, row, col) -> tuple:
+    super().get_moves(board, row, col)
+
+    valid_moves = []
+    invalid_moves = []
+    capture_moves = []
+    
+    # down
+    moves = [
+      (row + 1, col), 
+      (row - 1, col), 
+      (row + 1, col + 1), 
+      (row + 1, col - 1), 
+      (row - 1, col - 1), 
+      (row - 1, col + 1), 
+      (row, col - 1), 
+      (row, col + 1), 
+    ]
+
+    valid_moves = []
+    invalid_moves = []
+    capture_moves = []
+
+    for row, col in moves: 
+      # prevent out of range movement
+      if board.is_valid_position(row, col): 
+        if board.is_target_empty(row, col):
+          valid_moves.append((row, col))
+        else:
+          capture_moves.append((row, col))
+
+    return valid_moves, invalid_moves, capture_moves
